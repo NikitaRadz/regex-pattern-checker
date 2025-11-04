988fc85a934fd94f78ade44adfdfe8c63b3704e6
Nikita Radzevicius <nradzevicius10@gmail.com>
2025-11-04 12:29:15 +0000

    Remove yo code boilerplate code which added unneeded command and comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "regex-pattern-checker" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('regex-pattern-checker.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Regex Pattern Checker!');
-	});
-
-	context.subscriptions.push(disposable);
 
 	// Register the sidebar webview view provider
 	const provider = new RegexViewProvider(context);
@@ -27,9 +8,6 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.registerWebviewViewProvider(RegexViewProvider.viewId, provider)
 	);
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
 
 class RegexViewProvider implements vscode.WebviewViewProvider {
 		public static readonly viewId = 'regexPatternChecker';
